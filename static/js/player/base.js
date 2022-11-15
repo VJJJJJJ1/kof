d9bb20a7ca6bae2d9ad52b427f1c77ab8c613662
VJJJJJJ1 <1607945383@qq.com>
2022-11-15 18:16:41 +0800

    add images

--- static/js/player/base.js
+++ static/js/player/base.js
@@ -28,6 +28,11 @@ export class Player extends AcGameObject {
         this.pressed_keys = this.root.game_map.controller.pressed_keys;
 
         this.status = 3; // 0: idle， 1：向前， 2：向后， 3：跳跃， 4：攻击， 5：被打， 6：死亡
+        // 存动作
+        this.animations = new Map();
+
+        // 当前记录的帧数
+        this.frame_current_cnt = 0;
     }
 
     start() {
@@ -51,7 +56,8 @@ export class Player extends AcGameObject {
             space = this.pressed_keys.has('Enter');
         }
 
-        console.log(this.pressed_keys);
+        // console.log(this.pressed_keys);
+        console.log(this.status);
         if (this.status === 0 || this.status === 1) {
             if (w) {
                 // 三种起跳方式：
@@ -71,7 +77,7 @@ export class Player extends AcGameObject {
             }
             else if (a) {
                 this.vx = -this.speedx;
-                this.status = 2;
+                this.status = 1;
             }
             else {
                 this.vx = 0;
@@ -80,8 +86,51 @@ export class Player extends AcGameObject {
         }
     }
 
+    // update_controller() {
+    //     let w, a, d, space;
+    //     if (this.id === 0) {
+    //         w = this.pressed_keys.has('w');
+    //         a = this.pressed_keys.has('a');
+    //         d = this.pressed_keys.has('d');
+    //         space = this.pressed_keys.has(' ');
+    //     } else {
+    //         w = this.pressed_keys.has('ArrowUp');
+    //         a = this.pressed_keys.has('ArrowLeft');
+    //         d = this.pressed_keys.has('ArrowRight');
+    //         space = this.pressed_keys.has('Enter');
+    //     }
+
+    //     if (this.status === 0 || this.status === 1) {
+    //         if (w) {
+    //             if (d) {
+    //                 this.vx = this.speedx;
+    //             } else if (a) {
+    //                 this.vx = -this.speedx;
+    //             } else {
+    //                 this.vx = 0;
+    //             }
+    //             this.vy = -this.speedy;
+    //             this.status = 3;
+    //             this.frame_current_cnt = 0;
+    //         } else if (d) {
+    //             this.vx = this.speedx;
+    //             this.status = 1;
+    //         } else if (a) {
+    //             this.vx = -this.speedx;
+    //             this.status = 1;
+    //         } else {
+    //             this.vx = 0;
+    //             this.status = 0;
+    //         }
+    //     }
+    // }
+
+
     update_move() {
-        this.vy += this.gravity;
+        if (this.status === 3) {
+            this.vy += this.gravity;
+        }
+
 
         this.x += this.vx * this.timedelta / 1000;
         this.y += this.vy * this.timedelta / 1000;
@@ -109,7 +158,20 @@ export class Player extends AcGameObject {
     }
 
     render() {
-        this.ctx.fillStyle = this.color;
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        // this.ctx.fillStyle = this.color;
+        // this.ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        let status = this.status;
+
+        if (this.status === 1 && this.direction * this.vx < 0) status = 2;
+
+        let obj = this.animations.get(status);
+        if (obj && obj.loaded) {
+            let k = parseInt(this.frame_current_cnt / obj.frame_rate) % obj.frame_cnt;
+            let image = obj.gif.frames[k].image;
+            this.ctx.drawImage(image, this.x, this.y + obj.offset_y, image.width * obj.scale, image.height * obj.scale);
+
+        }
+        this.frame_current_cnt++;
     }
 }
